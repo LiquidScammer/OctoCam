82e57eae63ee8377a2de9d08d1470e8439c85cea
OctoBurr <132259285+OctoBurr@users.noreply.github.com>
2023-10-21 20:09:38 +0100

    plugin info for the epikness

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace OctoCam
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.octoburr.gorillatag.octocam";
+        public const string Name = "OctoCam";
+        public const string Version = "1.0.0";
+    }
+}
