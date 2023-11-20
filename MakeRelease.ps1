7ea68d479aad7176f03be293bb1b1651ada90cc2
OctoBurr <132259285+OctoBurr@users.noreply.github.com>
2023-11-20 07:28:19 +0000

    Delete MakeRelease.ps1

--- MakeRelease.ps1
+++
@@ -1,8 +0,0 @@
-
-$MyInvocation.MyCommand.Path | Split-Path | Push-Location
-$Name = (ls *.csproj).BaseName
-dotnet build -c Release
-mkdir BepInEx\plugins\$Name
-cp bin\Release\netstandard2.1\$Name.dll BepInEx\plugins\$Name\
-Compress-Archive .\BepInEx\ $Name-v
-rmdir .\BepInEx\ -Recurse
